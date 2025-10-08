5182af3d21de8b9ccc1c7dfe3b428da6e1b8c83d
IlkinEhmedov <ilkin656.u@gmail.com>
2025-10-08 12:18:30 +0400

    loading filename updated

--- app/about/loading.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./loading.module.scss"
-
-export default function Loading() {
-    return (
-        <div className={styles.overlay}>
-            <span className={styles.loader}></span>
-        </div>
-    )
-}
