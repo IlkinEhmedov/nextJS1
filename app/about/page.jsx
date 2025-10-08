84e0bbca0409f050c0d6941fc68a35888188a5e1
IlkinEhmedov <ilkin656.u@gmail.com>
2025-10-08 12:10:07 +0400

    loading updaed

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,10 +3,6 @@ import { Box, Card, Grid } from "@mui/material"
 export default async function News() {
     const products = await fetch('https://chatapp-znz3.onrender.com/api/v1/auth/find-all', { cache: 'no-store' })
         .then(x => x.json())
-    const products2 = await fetch('https://chatapp-znz3.onrender.com/api/v1/auth/find-all', { cache: 'force-cache' })
-        .then(x => x.json())
-    const products3 = await fetch('https://chatapp-znz3.onrender.com/api/v1/auth/find-all', { next: { revalidate: 60 } })
-        .then(x => x.json())
 
     return (
         <Box sx={{ padding: 4 }}>
@@ -14,34 +10,6 @@ export default async function News() {
             <h2>SSR</h2>
             <Grid container spacing={2}>
                 {products?.map(item => (
-                    <Grid size={{ md: 4, sm: 6, xs: 12 }} item key={item._id}>
-                        <Card sx={{ padding: 2 }}>
-                            <h1>{item.username}</h1>
-                            <p>
-                                {item.count}
-                            </p>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-            <hr /><br /><br />
-            <h2>SSG</h2>
-            <Grid container spacing={2}>
-                {products2?.map(item => (
-                    <Grid size={{ md: 4, sm: 6, xs: 12 }} item key={item._id}>
-                        <Card sx={{ padding: 2 }}>
-                            <h1>{item.username}</h1>
-                            <p>
-                                {item.count}
-                            </p>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-            <hr /><br /><br />
-            <h2>ISR</h2>
-            <Grid container spacing={2}>
-                {products3?.map(item => (
                     <Grid size={{ md: 4, sm: 6, xs: 12 }} item key={item._id}>
                         <Card sx={{ padding: 2 }}>
                             <h1>{item.username}</h1>
